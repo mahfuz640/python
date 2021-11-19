778c8ea57dc2b1f0c4578e6801b030c6c84d6460
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-11-19 19:17:55 +0600

    tupple

---
+++ tupple test.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #tupple
+>>> a=()
+>>> a
+()
+>>> type(a)
+<class 'tuple'>
+>>> a=('onion','potato','ginger','cucumber')
+>>> 
+>>> a
+('onion', 'potato', 'ginger', 'cucumber')
+>>> type(a)
+<class 'tuple'>
+>>> #() first braket a rakha ke tupple bole tupple change kora jai na
+>>> 
